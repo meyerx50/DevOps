66045c12de4f4e1dc7beeb4d4d3fbc5c8bd62768
meyerx50 <eduardomeyer@outlook.com>
2020-11-17 16:17:01 +0100

    First commit

--- asymkey.py
+++ asymkey.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=4096, backend=backend())
-public_key = private_key.public_key
+#public_key = private_key.public_key
 public_key = private_key.public_key()
 
 message = b"More secrets go here"
---
+++ executable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def say_it():
+    greeting = 'Hello'
+    target = 'Joe'
+    message = greeting + target
+    print(message)
+
+
+if __name__ == '__main__':
+    say_it()
+
+#chmod +x say_it.py
