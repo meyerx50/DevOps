584ca7fe7af337bf5cd51ffbe2c610d74b719c53
brguy <mail.meyer@gmai.com>
2020-12-29 23:15:45 +0100

    Logic pushed into the class

--- ch04-game.py
+++ ch04-game.py
@@ -4,9 +4,10 @@ class Human:
 
     def __init__(self):
         self.data = []
-        self.life_points = 100
+        self.life_points = 0
         self.sword_skills = 0
         self.shield_skills = 0
+        self.name = "Undefined"
 
     def attack(self):
         damage = random.randint(1, self.sword_skills)
@@ -16,23 +17,31 @@ class Human:
         block = random.randint(1, self.shield_skills)
         return block
 
+    def damage(self, attack):
+        result = attack - self.defense()
+        if (result > 0):
+            self.life_points -= result
+        print(f'{self.name} has now {self.life_points}.')
+
+    def alive(self):
+        if self.life_points > 0:
+            return True
+        else:
+            print(f'{self.name} is now dead')
+            return False
+
 Knight = Human()
+Knight.name = "Bolsonaro"
 Knight.life_points = 100
-Knight.sword_skills = 10
-Knight.shield_skills = 10
+Knight.sword_skills = 15
+Knight.shield_skills = 5
 
 Amazon = Human()
+Amazon.name = "Dilma"
 Amazon.life_points = 50
-Amazon.sword_skills = 15
-Amazon.shield_skills = 20
+Amazon.sword_skills = 10
+Amazon.shield_skills = 25
 
-while Amazon.life_points > 0 and Knight.life_points > 0:
-    damage = Knight.attack() - Amazon.defense()
-    if damage >= 0:
-        Amazon.life_points -= damage
-    print(f'Amazon has now {Amazon.life_points}')
-    damage = Amazon.attack() - Knight.defense()
-    if damage >= 0:
-        Knight.life_points -= damage
-    print(f'Knight has now {Knight.life_points}')
-
+while Amazon.alive() and Knight.alive():
+    Knight.damage(Amazon.attack())
+    Amazon.damage(Knight.attack())
