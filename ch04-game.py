c894682574cb7c725855df5b4072422294f75420
brguy <mail.meyer@gmai.com>
2020-12-30 11:05:16 +0100

    Cure magic added

--- ch04-game.py
+++ ch04-game.py
@@ -19,39 +19,69 @@ class Human:
 
     def __init__(self):
         self.data = []
-        self.life_points = 0
+        self.life_points_max = 0
+        self.life_points_current = 0
+        self.mana_points_max = 0
+        self.mana_points_current = 0
         self.name = "Undefined"
         self.left_hand = ""
         self.right_hand = ""
         # How good the player can handle weapons. Increases attack and defense
         self.weapon_skills = 0
+        # How good the player can use magic skills (e.g. Cure)
+        self.magic_skills = 0
+        # Required amount of mana to use cure
+        self.mana_for_cure = 0
+        # Factor to calculate the amount of life points got through cure
+        self.life_per_cure = 0.0
 
+    # Calculates a random damage value a player can delivered based on weapons and skills
     def attack(self):
         # Sum all weapons attack points and multiply by the skill factor
         total_attack = round((self.right_hand.attack + self.left_hand.attack) * self.weapon_skills)
         damage = random.randint(1, total_attack)
         return damage
 
+    # Calculates a random block value a player can reduce from an attack based on weapons and skills
     def defense(self):
         # Sum all weapons defense points and multiply by the skill factor
         total_defense = round((self.right_hand.defense + self.left_hand.defense) * self.weapon_skills)
         block = random.randint(1, total_defense)
         return block
 
-    def damage(self, Attacker):
-        result = Attacker.attack() - self.defense()
-        if (result > 0):
-            self.life_points -= result
-            print(f'{self.name} suffered an attack from {Attacker.name} with damage'
-                  f' of {result} hit points and has now {self.life_points} life points.')
-        else:
-            print(f'{self.name} successfully blocked an attack from {Attacker.name}.')
+    # Delivers damage to a victim
+    def engage(self, Victim):
+        if self.alive():
+            if Victim.alive():
+                result = self.attack() - Victim.defense()
+                if (result > 0):
+                    Victim.life_points_current -= result
+                    print(f'{Victim.name} suffered an attack from {self.name} with damage'
+                          f' of {result} hit points and has now {Victim.life_points_current} life points.')
+                    if Victim.alive() == False:
+                        print(f'{Victim.name} is now dead')
+                else:
+                    print(f'{self.name} successfully blocked an attack from {Victim.name}.')
 
+    # Restore life points for the player him/herself
+    def cure(self):
+        if self.alive():
+            if (self.mana_points_current >= self.mana_for_cure) or self.life_points_current > 0:
+                self.mana_points_current -= self.mana_for_cure
+                total_cure = round(self.life_points_max * random.uniform(0.1, self.life_per_cure))
+                if (self.life_points_current + total_cure) > self.life_points_max:
+                    self.life_points_current = self.life_points_max
+                else:
+                    self.life_points_current += total_cure
+                print(f'{self.name} has conjured the cure spell and restored {total_cure} life points.')
+            else:
+                print(f'Not enough mana or already dead.')
+
+    # Is the player still alive?
     def alive(self):
-        if self.life_points > 0:
+        if self.life_points_current > 0:
             return True
         else:
-            print(f'{self.name} is now dead.')
             return False
 
 fst_bare = Weapon()
@@ -80,19 +110,33 @@ shd_demon.defense = 20
 
 Knight = Human()
 Knight.name = "Johnny Lawrence"
-Knight.life_points = 101
+Knight.life_points_max = 100
+Knight.life_points_current = Knight.life_points_max
+Knight.mana_points = 50
 Knight.right_hand = swd_justice
 Knight.left_hand = shd_guardian
 Knight.weapon_skills = 1.1
+Knight.mana_for_cure = 10
+Knight.life_per_cure = 0.3
 
 Hunter = Human()
 Hunter.name = "Daniel Larusso"
-Hunter.life_points = 50
+Hunter.life_points_max = 100
+Hunter.life_points_current = Hunter.life_points_max
 Hunter.right_hand = fst_bare
 Hunter.left_hand = shd_demon
 Hunter.weapon_skills = 24.2
+Hunter.mana_for_cure = 10
+Hunter.life_per_cure = 0.2
 
 
 while Hunter.alive() and Knight.alive():
-    Knight.damage(Hunter)
-    Hunter.damage(Knight)
+
+    Knight.engage(Hunter)
+    # Conjure cure when life points are below 50%
+    if Hunter.life_points_current <= round(Hunter.life_points_max * 0.5):
+        Hunter.cure()
+
+    Hunter.engage(Knight)
+    if Knight.life_points_current <= round(Knight.life_points_max * 0.5):
+        Knight.cure()
