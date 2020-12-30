f8fb29cd0c3550f7e3117c3a5309eaf31aaa368b
brguy <mail.meyer@gmai.com>
2020-12-30 09:39:06 +0100

    Including weapons

--- ch04-game.py
+++ ch04-game.py
@@ -1,61 +1,81 @@
 import random
 
+weapon_type = ['Fist', 'Axe', 'Sword', 'Shield', 'Wand']
+
+class Weapon:
+
+    def __init__(self):
+        self.data = []
+        self.name = "Undefined"
+        self.attack = 0
+        self.defense = 0
+        self.type = weapon_type[0]
+
+
 class Human:
 
     def __init__(self):
         self.data = []
         self.life_points = 0
-        self.sword_skills = 0
-        self.shield_skills = 0
         self.name = "Undefined"
+        self.left_hand = ""
+        self.right_hand = ""
 
     def attack(self):
-        damage = random.randint(1, self.sword_skills)
+        damage = random.randint(1, self.right_hand.attack)
         return damage
 
     def defense(self):
-        block = random.randint(1, self.shield_skills)
+        block = random.randint(1, self.left_hand.defense)
         return block
 
-    def damage(self, attack):
-        result = attack - self.defense()
+    def damage(self, Attacker):
+        result = Attacker.attack() - self.defense()
         if (result > 0):
             self.life_points -= result
-        # print(f'{self.name} has now {self.life_points}.')
+            print(f'{self.name} suffered an attack from {Attacker.name} with damage'
+                  f' of {result} hit points and has now {self.life_points} life points.')
+        else:
+            print(f'{self.name} successfully blocked an attack from {Attacker.name}.')
 
     def alive(self):
         if self.life_points > 0:
             return True
         else:
-            # print(f'{self.name} is now dead')
+            print(f'{self.name} is now dead.')
             return False
 
-games = 100000
-amazon_wins = 0
-knight_wins = 0
+fst_bare = Weapon()
+fst_bare.name = "Bare Fist"
+fst_bare.type = weapon_type[0]
+fst_bare.attack = 2
+fst_bare.defense = 1
 
-for x in range(games):
-    Knight = Human()
-    Knight.name = "Bolsonaro"
-    Knight.life_points = 101
-    Knight.sword_skills = 15
-    Knight.shield_skills = 5
+swd_justice = Weapon()
+swd_justice.name = "Great Sword of Justice"
+swd_justice.type = weapon_type[2]
+swd_justice.attack = 10
+swd_justice.defense = 2
 
-    Amazon = Human()
-    Amazon.name = "Dilma"
-    Amazon.life_points = 50
-    Amazon.sword_skills = 10
-    Amazon.shield_skills = 26
+shd_guardian = Weapon()
+shd_guardian.name = "Guardian Shield"
+shd_guardian.type = weapon_type[2]
+shd_guardian.attack = 1
+shd_guardian.defense = 15
 
-    while Amazon.alive() and Knight.alive():
-        Knight.damage(Amazon.attack())
-        Amazon.damage(Knight.attack())
+Knight = Human()
+Knight.name = "Johnny Lawrence"
+Knight.life_points = 101
+Knight.right_hand = swd_justice
+Knight.left_hand = shd_guardian
 
-    if Amazon.alive():
-        amazon_wins += 1
-    else:
-        knight_wins +=1
+Hunter = Human()
+Hunter.name = "Daniel Larusso"
+Hunter.life_points = 50
+Hunter.right_hand = fst_bare
+Hunter.left_hand = fst_bare
 
-print(f'{Amazon.name} won {round(amazon_wins * 100 / games, 2)} % of the '
-      f'times, while {Knight.name} won {round(knight_wins * 100 / games, 2)} '
-      f'% of the times')
+
+while Hunter.alive() and Knight.alive():
+    Knight.damage(Hunter)
+    Hunter.damage(Knight)
