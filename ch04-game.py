02af0e9e5ede4ead7b39090e1c3a55cc97dd69a0
brguy <mail.meyer@gmai.com>
2021-01-01 20:43:09 +0100

    Introducing runes

--- ch04-game.py
+++ ch04-game.py
@@ -22,15 +22,17 @@ class Spell:
         self.spell_power = 0.0
         # What is the base to calculate the spell outcome
         self.point_base = 0
-        self.required_mana = 0
+        # How many mana points to use it?
+        self.mana_cost = 0
 
-    def attack(self, magic_level):
+    # All spells are a mean of the spell power and the player magic level
+    def attack_damage(self, magic_level):
         damage_power = self.point_base * self.spell_power
         damage_skill = self.point_base * magic_level
         balanced_damage = statistics.mean([damage_skill, damage_power])
         return round(balanced_damage)
 
-    def defense(self, magic_level):
+    def defense_block(self, magic_level):
         blocked_power = self.point_base * self.spell_power
         blocked_skill = self.point_base * magic_level
         balanced_blocked = statistics.mean([blocked_power, blocked_skill])
@@ -42,6 +44,9 @@ class Spell:
         balanced_cure = statistics.mean([cure_power, cure_skill])
         return round(balanced_cure)
 
+class Rune(Spell):
+    def __init__(self):
+        self.charges = 0
 
 class Weapon:
 
@@ -58,36 +63,36 @@ class Weapon:
 class Creature:
 
     def __init__(self):
+        # Basics
         self.data = []
-        self.life_points_max = 0
-        self.life_points_current = 0
-        self.mana_points_max = 0
-        self.mana_points_current = 0
         self.name = "Undefined"
-        self.left_hand = ""
-        self.right_hand = ""
-        # How good the player can handle weapons. Increases attack and defense
-        self.weapon_skills = 0.0
-        # How good the player can use magic skills (e.g. Cure)
-        self.magic_level = 0.0
-        # Required amount of mana to use cure
-        self.mana_for_cure = 0
-        # Factor to calculate the amount of life points got through cure
-        self.life_per_cure = 0.0
         self.can_attack = False
         self.can_defend = False
+        # Life
+        self.life_points_max = 0
+        self.life_points_current = 0
+        # Magic
+        self.mana_points_max = 0
+        self.mana_points_current = 0
         self.magic_skill_set = []
+        # > How good the player can use magic skills (e.g. Cure)
+        self.magic_level = 0.0
+        # Weapons
+        self.left_hand = ""
+        self.right_hand = ""
+        # > How good the player can handle weapons. Increases attack and defense
+        self.weapon_skills = 0.0
 
 
     # Calculates a random damage value a player can delivered based on weapons and skills
-    def attack(self):
+    def attack_damage(self):
         # Sum all weapons attack points and multiply by the skill factor
         total_attack = round((self.right_hand.damage + self.left_hand.damage) * self.weapon_skills)
         damage = random.randint(1, total_attack)
         return damage
 
     # Calculates a random block value a player can reduce from an attack based on weapons and skills
-    def defense(self):
+    def defense_block(self):
         # Sum all weapons defense points and multiply by the skill factor
         total_defense = round((self.right_hand.protection + self.left_hand.protection) * self.weapon_skills)
         block = random.randint(1, total_defense)
@@ -102,10 +107,10 @@ class Creature:
                 if damage >= 0:
                     result = damage
                 else:
-                    result = self.attack()
+                    result = self.attack_damage()
                 # Result can be minimized if the victim is able to defend him/herself
                 if victim.can_defend:
-                    result -= victim.defense()
+                    result -= victim.defense_block()
                 # Any harm?
                 if (result > 0):
                     victim.life_points_current -= result
@@ -133,7 +138,7 @@ class Creature:
     # Can the play cast this spell?
     def can_cast(self, spell):
         if self.magic_level >= spell.required_magic_skill:
-            if self.mana_points_current >= spell.required_mana:
+            if self.mana_points_current >= spell.mana_cost:
                 return True
             else:
                 return False
@@ -141,7 +146,7 @@ class Creature:
             return False
 
     # Cast a magic spell
-    def cast_spell(self, spell, victim):
+    def use_magic(self, spell, victim):
         if self.can_cast(spell):
             # Cure spells
             if spell.type == 0:
@@ -150,10 +155,11 @@ class Creature:
                     self.life_points_current = self.life_points_max
                 else:
                     self.life_points_current += cure
+                self.mana_points_current -= spell.mana_cost
                 print(f'{self.name} has cured {cure} life points and has now {self.life_points_current}')
             # Attack spells
             elif spell.type == 1:
-                damage = spell.attack(self.magic_level)
+                damage = spell.attack_damage(self.magic_level)
                 self.engage(victim, damage)
         else:
             print(f'{self.name} cannot cast the spell {spell.name}.')
@@ -161,7 +167,7 @@ class Creature:
 ### Game ###
 # Creating game objects
 
-# Creating skills
+# Creating magic spells
 spl_cure = Spell()
 spl_cure.name = "Light Cure"
 spl_cure.description = "Restores a small amount of life points"
@@ -169,7 +175,7 @@ spl_cure.type = 0
 spl_cure.required_magic_skill = 0.1
 spl_cure.spell_power = 1.2
 spl_cure.point_base = 10
-spl_cure.required_mana = 5
+spl_cure.mana_cost = 5
 
 spl_death = Spell()
 spl_death.name = "Sudden Death"
@@ -178,7 +184,7 @@ spl_death.type = 1
 spl_death.required_magic_skill = 3.0
 spl_death.spell_power = 1.9
 spl_death.point_base = 20
-spl_death.required_mana = 25
+spl_death.mana_cost = 25
 
 # Creating weapons
 fst_bare = Weapon()
@@ -211,35 +217,52 @@ paw_rabbit.type = weapon_type[2]
 paw_rabbit.damage = 0
 paw_rabbit.protection = 0
 
+# Creating runes
+# Runes are magic objects that can be only used a few times and require no mana
+run_frozen = Rune()
+run_frozen.name = "Frozen Avalanche Rune"
+run_frozen.description = "Freezes your enemy's attack"
+run_frozen.type = 2
+run_frozen.required_magic_skill = 3.1
+run_frozen.spell_power = 4
+run_frozen.point_base = 1000
+run_frozen.charges = 5
+
 # Creating Players
+# > Basics
 Knight = Creature()
 Knight.name = "Johnny Lawrence"
+Knight.can_attack = True
+Knight.can_defend = True
+# > Life
 Knight.life_points_max = 100
 Knight.life_points_current = Knight.life_points_max
-Knight.mana_points = 50
+# > Weapons
 Knight.right_hand = swd_justice
 Knight.left_hand = shd_guardian
 Knight.weapon_skills = 1.1
-Knight.mana_for_cure = 10
-Knight.life_per_cure = 0.3
-Knight.can_attack = True
-Knight.can_defend = True
+# > Magic
+Knight.magic_level = 5
+Knight.mana_points_max = 50
+Knight.mana_points_current = 50
 Knight.magic_skill_set = [spl_cure]
 
+# > Basics
 Hunter = Creature()
 Hunter.name = "Daniel Larusso"
+Hunter.can_attack = True
+Hunter.can_defend = True
+# > Life
 Hunter.life_points_max = 100
 Hunter.life_points_current = Hunter.life_points_max
+# > Weapons
 Hunter.right_hand = fst_bare
 Hunter.left_hand = shd_demon
 Hunter.weapon_skills = 10.2
+# > Magic
 Hunter.magic_level = 5
 Hunter.mana_points_max = 50
 Hunter.mana_points_current = 50
-Hunter.mana_for_cure = 10
-Hunter.life_per_cure = 0.2
-Hunter.can_attack = True
-Hunter.can_defend = True
 Hunter.magic_skill_set = [spl_cure, spl_death]
 
 Rabbit = Creature()
@@ -253,6 +276,7 @@ Rabbit.can_defend = False
 
 # Fight!
 Hunter.engage(Knight, -1)
-Hunter.cast_spell(Hunter.magic_skill_set[0], None)
-Hunter.cast_spell(Hunter.magic_skill_set[1], Knight)
-
+Hunter.use_magic(Hunter.magic_skill_set[1], Knight)
+Hunter.engage(Knight, -1)
+Knight.use_magic(Knight.magic_skill_set[0], None)
+Hunter.use_magic(Hunter.magic_skill_set[1], Knight)
